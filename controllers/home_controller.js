30a53e5cdc0c31398c679a6c3b53542887f828eb
Nakul <nakulgarg1424@gmail.com>
2023-06-21 22:32:58 +0530

    cr

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,18 +1,26 @@
-const User = require('../models/user') ; 
-const Product = require('../models/product') ; 
+const User = require('../models/user');
+const Product = require('../models/product');
 const { findById } = require('../models/user');
 
-module.exports.homeload = function( req , res ){
-    res.render('home' , {
-        title : "Home"
+module.exports.homeload = function (req, res) {
+    res.render('home', {
+        title: "Home"
     })
 }
-module.exports.productupload = function( req , res ){
-    res.render('product_list' , {
-        title : "LIST || PRODUCT"
+module.exports.productupload = function (req, res) {
+    res.render('product_list', {
+        title: "LIST || PRODUCT"
     })
 }
 
-module.exports.createproduct = function( req , res ){
-    
+
+module.exports.createproduct = function (req, res) {
+    Product.create({
+        productprice : req.body.productprice , 
+        productname : req.body.productname ,
+        
+        userofproduct: req.user._id
+    }
+    )
+    return res.redirect('back');
 }
