98894ad0d0c2656df6d2b153605bf926afe708a1
Nakul <nakulgarg1424@gmail.com>
2023-06-21 20:37:04 +0530

    SOme

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,18 @@
-const User = require('../models/user')
+const User = require('../models/user') ; 
+const Product = require('../models/product') ; 
+const { findById } = require('../models/user');
 
 module.exports.homeload = function( req , res ){
     res.render('home' , {
         title : "Home"
     })
 }
+module.exports.productupload = function( req , res ){
+    res.render('product_list' , {
+        title : "LIST || PRODUCT"
+    })
+}
 
+module.exports.createproduct = function( req , res ){
+    
+}
--- models/product.js
+++ models/product.js
@@ -1,44 +1,49 @@
 const mongoose = require('mongoose');
 // pridct have photo name and price 
 
-// const multer = require('multer') ; 
-// const path = require('path') ; 
-// const AVATAR_PATH = path.join('/uploads/users/avatars') ; 
+const multer = require('multer') ; 
+const path = require('path') ; 
+const User = require('./user');
+const PRODUCT_PATH = path.join('/uploads/products') ; 
 const productschema = new mongoose.Schema({
-    name:{
+    productname:{
         type: String,
         required: true ,
     },
-    price:{
+    userofproduct: { 
+        type :mongoose.Schema.Types.ObjectId ,
+        ref : 'User' , 
+        required : true 
+    },
+    productprice:{
         type : String,
         require: true
     },
-    // avatar :{
-    //     type: String,
-         
-    // }
+    productimg :{
+        type: String,
+    }
 }, {timestamps: true 
 });
 
-// let storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null,path.join(__dirname , '..'  , AVATAR_PATH)) ; 
-//     },
-//     filename: function (req, file, cb) {
-//     //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//     //   cb(null, file.fieldname + '-' + uniqueSuffix)
-//     cb(null, file.fieldname + '-' + Date.now()) ;
-//     }
-//   });
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,path.join(__dirname , '..'  , PRODUCT_PATH)) ; 
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    cb(null, file.fieldname + '-' + Date.now()) ;
+    }
+  });
 
 //static funcinto here for the user ther can bu meultiple other wasys 
 
-// userschema.statics.uploadedAvatar = multer({ storage : storage}).single('avatar') ;
-// userschema.statics.avatarPath = AVATAR_PATH ; 
-// console.log(userschema.statics.avatarPath) ; 
+productschema.statics.uploadedProduct = multer({ storage : storage}).single('productimg') ;
+productschema.statics.productPath = PRODUCT_PATH ; 
+// console.log(productschema.statics.productPath) ; 
 
 
 // now write controler code 
 
-const User  = mongoose.model('User' , userschema);
-module.exports = User ;
+const Product  = mongoose.model('Product' , productschema);
+module.exports = Product ;
