e4ab0706e9a05e4af1332aa7199a3599210ce0e3
Nakul <nakulgarg1424@gmail.com>
2023-06-21 21:09:07 +0530

    crt

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const multer = require('multer') ; 
-const path = require('path') ; 
-const AVATAR_PATH = path.join('/uploads/users/avatars') ; 
+// const multer = require('multer') ; 
+// const path = require('path') ; 
+// const AVATAR_PATH = path.join('/uploads/users/avatars') ; 
 const userschema = new mongoose.Schema({
     email:{
         type: String,
@@ -16,10 +16,10 @@ const userschema = new mongoose.Schema({
         type: String,
         required :true
     },
-    avatar :{
-        type: String,
-         
-    } ,
+    // avatar :{
+        // type: String,
+        //  
+    // } ,
     contact:{
         type: String , 
         required:true 
@@ -31,22 +31,22 @@ const userschema = new mongoose.Schema({
 }, {timestamps: true 
 });
 
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null,path.join(__dirname , '..'  , AVATAR_PATH)) ; 
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    cb(null, file.fieldname + '-' + Date.now()) ;
-    }
-  });
+// let storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null,path.join(__dirname , '..'  , AVATAR_PATH)) ; 
+//     },
+//     filename: function (req, file, cb) {
+//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//       cb(null, file.fieldname + '-' + uniqueSuffix)
+//     cb(null, file.fieldname + '-' + Date.now()) ;
+//     }
+//   });
 
 //static funcinto here for the user ther can bu meultiple other wasys 
 
-userschema.statics.uploadedAvatar = multer({ storage : storage}).single('avatar') ;
-userschema.statics.avatarPath = AVATAR_PATH ; 
-console.log(userschema.statics.avatarPath) ; 
+// userschema.statics.uploadedAvatar = multer({ storage : storage}).single('avatar') ;
+// userschema.statics.avatarPath = AVATAR_PATH ; 
+// console.log(userschema.statics.avatarPath) ; 
 
 
 // now write controler code 
