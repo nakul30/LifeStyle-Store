66f435ea17f1caac8b7c7faa0622d4c06c00480f
Nakul <nakulgarg1424@gmail.com>
2023-06-22 16:25:48 +0530

    Products Shown

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,39 +3,12 @@ const User = require('../models/user');
 const Product = require('../models/product');
 
 module.exports.homeload = function (req, res) {
-  res.render('home', {
-    title: "Home"
+  Product.find({}).populate('puser').exec()
+  .then(products =>{
+    res.render('home' , {
+      title:"HOME" , 
+      productsby : products
+    })
   })
 }
-module.exports.home = function(req, res) {
-  // Find all users
-  User.find({})
-    .exec()
-    .then(users => {
-      // Find all posts
-      Post.find({})
-        .populate('user') // Populate the 'user' field with User documents
-        .populate({
-          path: 'comments',
-          populate: {
-            path: 'user'
-          }
-        })
-        .exec()
-        .then(posts => {
-          return res.render('home', {
-            title: "Universe Home",
-            posts: posts,
-            all_users: users
-          });
-        })
-        .catch(err => {
-          console.error(err);
-          // Handle the error, such as sending an error response or redirecting to an error page
-        });
-    })
-    .catch(err => {
-      console.error(err);
-      // Handle the error, such as sending an error response or redirecting to an error page
-    });
-};
+// we only have user id if we dont prepopulate product with user
